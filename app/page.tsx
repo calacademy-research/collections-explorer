29faa2f4824fa808e8dc5ce46c8784eeae337e2d
Matthew Wang <matt8p@outlook.com>
2025-03-24 20:35:24 -0700

    app

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return <div className="bg-amber-200 w-full">Home fdasfdasfa</div>;
+  redirect("/botany");
 }
